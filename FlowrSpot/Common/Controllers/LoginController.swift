f82b7af3accf8f809964349b1d54b43cff20ad01
mevalidjobop <mevalidjobop@gmail.com>
2021-12-04 09:29:09 +0100

    Login controller initial

---
+++ FlowrSpot/Common/Controllers/LoginController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginController.swift
+//  FlowrSpot
+//
+//  Created by Mirela V on 04/12/2021.
+//  Copyright © 2021 Povio Labs. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+  }
+}
